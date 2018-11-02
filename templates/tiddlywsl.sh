68b80f33667165ab5ddf3b65d294bd701426e962
Jonathan Freedman <jonafree@gmail.com>
2018-11-02 09:16:50 -0700

    better wsl quasi init script

--- templates/tiddlywsl.sh
+++ templates/tiddlywsl.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-TIDDLYWIKI_PID="{{tiddlywiki_pid_directory}}/{{item.name}}"
+TIDDLYWIKI_PIDFILE="{{tiddlywiki_pid_directory}}/{{item.name}}"
 TIDDLYWIKI_LOG="{{tiddlywiki_log_directory}}/{{item.name}}"
 TIDDLYWIKI_DATA="{{item.path}}"
 TIDDLYWIKI_PORT="{{item.port}}"
@@ -14,42 +14,52 @@ usage() {
     exit 1
 }
 
+check_pid() {
+    if [ -e "$TIDDLYWIKI_PIDFILE" ] ; then
+        TIDDLWIKI_PID="$(cat "$TIDDLYWIKI_PIDFILE")"
+        if [ ! -z "$TIDDLYWIKI_PID" ] && ps "$TIDDLYWIKI_PID" &> /dev/null ; then
+            return
+        else
+            TIDDLYWIKI_PID=""
+            rm "$TIDDLYWIKI_PIDFILE"
+        fi
+    fi
+    
+}
+
 do_start() {
-    if [ ! -f "$TIDDLYWIKI_PID" ] ; then
+    check_pid
+    if [ ! -z "$TIDDLYWIKI_PID" ] ; then
+        echo "{{item.name}} already running $(cat "$TIDDLYWIKI_PIDFILE")"
+    else
         nohup tiddlywiki "$TIDDLYWIKI_DATA" \
               --server "$TIDDLYWIKI_PORT" \
             &> "$TIDDLYWIKI_LOG" &
         TPID="$!"
         if ps "$TPID" &> /dev/null ; then
-            echo "$TPID" > "$TIDDLYWIKI_PID"
+            echo "$TPID" > "$TIDDLYWIKI_PIDFILE"
             echo "Started {{item.name}}"
         else
             echo "Unable to start {{item.name}}"
             exit 1
         fi
-    else
-        echo "{{item.name}} already running $(cat "$TIDDLYWIKI_PID")"
     fi
 }
 
 do_stop() {
-    if [ -f "$TIDDLYWIKI_PID" ] ; then
-        TPID="$(cat "$TIDDLYWIKI_PID")"
-        if ps "$TPID" &> /dev/null ; then
-            kill "$TPID"
-            echo "Stopped {{item.name}}"
-        else
-            echo "{{item.name}} crashed"
-        fi
-        rm "$TIDDLYWIKI_PID"
+    check_pid
+    if [ ! -z "$TIDDLYWIKI_PID" ] ; then
+        kill "$TIDDLYWIKI_PID"
+        echo "Stopped {{item.name}}"
+        rm "$TIDDLYWIKI_PIDFILE"        
     else
         echo "{{item.name}} is not running"
     fi
 }
 
 do_status() {
-    if [ -f "$TIDDLYWIKI_PID" ] ; then
-        TPID="$(cat "$TIDDLYWIKI_PID")"
+    if [ -f "$TIDDLYWIKI_PIDFILE" ] ; then
+        TPID="$(cat "$TIDDLYWIKI_PIDFILE")"
         if ps "$TPID" &> /dev/null ; then
             echo "{{item.name}} running"
         else
